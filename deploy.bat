bba91784cd7b5663af7042d94894f21032dbcf59
Your Name <your-email@example.com>
2025-05-21 09:38:17 +0800

    Initial commit for Othello Five

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "C:\Users\Administrator\Desktop\游戏类\游戏类\游戏类网站编写模板4"
+git init
+git add .
+git commit -m "Initial commit for Othello Five"
+git remote add origin https://github.com/lz-215/Othello-Five.git
+git push -u origin master
+pause
